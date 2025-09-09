6f3c978bc2dfbe49010dc61b75b1c69f081f78b6
yashika <yashika9654@gmail.com>
2025-09-09 23:01:43 +0530

    review authorisation

--- models/review.js
+++ models/review.js
@@ -12,5 +12,9 @@ const reviewSchema=new Schema({
         type:Date,
         default:Date.now(),
     },
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    },
 });
 module.exports=mongoose.model("Review",reviewSchema);
