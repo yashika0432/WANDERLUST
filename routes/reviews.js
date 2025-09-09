2a7fd6597d6939aba6fa521ac0cc5dcb4830774c
yashika <yashika9654@gmail.com>
2025-09-09 23:05:45 +0530

    review authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,16 +3,16 @@ const router=express.Router({mergeParams:true});
 const wrapAsync=require("../utils/wrapasync.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
-const {validateReview}=require("../middleware.js");
+const {validateReview, isLoggedIn}=require("../middleware.js");
 
 
 
 
-router.post("/",validateReview,wrapAsync( async(req,res)=>{
-    console.log(req.params.id);
+router.post("/",isLoggedIn,validateReview,wrapAsync( async(req,res)=>{
     let {id}=req.params;
     let listing=await Listing.findById(id);
     let newReview=new Review(req.body.review);
+    newReview.author=req.user._id;
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
